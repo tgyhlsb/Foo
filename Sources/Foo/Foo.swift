a4b4bb6cfc334d9f2ad5ede43d1c95afbb4f011d
Tanguy Helesbeux <johnsmith@gmail.com>
2022-03-22 15:48:37 +0100

    use Apollo

--- Sources/Foo/Foo.swift
+++ Sources/Foo/Foo.swift
@@ -1,6 +1,10 @@
+import Apollo
+
 public struct Foo {
     public private(set) var text = "Hello, World!"
 
     public init() {
+        let cache = InMemoryNormalizedCache()
+        print("Foo library can allocate \(cache)")
     }
 }
